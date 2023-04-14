8d5ca85a31b888b0b257a727746b0f7092e4df50
Roman Shevyakov <roman@mgstr.ru>
2023-04-14 10:00:54 +0600

    feat: resolve cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function countCats(matr) {
+  let r = [];
+  for (item of matr) {
+    let count = item.filter((i) => i === '^^');
+    r.push(...count);
+  };
+  return r.length;
 }
+
+// console.log(countCats([
+//   [0, 1, '^^'],
+//   [0, '^^', 2],
+//   ['^^', 1, 2]
+// ]))
+
+
+
 
 module.exports = {
   countCats
